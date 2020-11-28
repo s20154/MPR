febaa8dea0023d7d9f408d347de8686a147b6092
Kijańczuk, Damian <s20154@pjwstk.edu.pl>
2020-11-28 19:58:20 +0100

    Revert "zjazd 3 part 2"
    This reverts commit 2ddeb7ff7e4c34c043916f526f90dc4a99dccb6b.

--- src/main/java/pl/pjatk/gameplay/Service/DamageService.java
+++ src/main/java/pl/pjatk/gameplay/Service/DamageService.java
@@ -5,7 +5,6 @@ import pl.pjatk.gameplay.Repository.PlayerRepository;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.Random;
 
 public class DamageService {
     private PlayerRepository playerRepository;
@@ -16,51 +15,4 @@ public class DamageService {
         defender.setHealth(defender.getHealth() - attacker.getAttack());
         return defender;
     }
-
-    public Player heal(Player player){
-        player.setHealth(player.getHealth() + 10);
-        return player;
-    }
-
-    public Player swapStats(Player player){
-        int tmp = player.getAttack();
-        player.setAttack(player.getHealth());
-        player.setHealth(tmp);
-        return player;
-    }
-
-    public Player berserk(Player player){
-        if(player.getHealth() < 5) {
-            player.setAttack(player.getAttack() * 2);
-        } else {
-            player.setHealth(player.getHealth() - 10);
-        }
-        return player;
-    }
-
-    public Player rollDice(Player player){
-        Random rand = new Random();
-        switch (rand.nextInt(6)){
-            case 0:
-                player.setHealth(player.getHealth()+20);
-                break;
-            case 1:
-                player.setHealth(10);
-                break;
-            case 2:
-                player.setAttack(player.getAttack()+15);
-                break;
-            case 3:
-                player.setAttack(5);
-                break;
-            case 4:
-                player.setAttack(player.getHealth());
-                break;
-            case 5:
-                player.setHealth(0);
-                break;
-        }
-    }
 }
-
-
--- src/test/java/pl/pjatk/gameplay/GameplayApplicationTests.java
+++ src/test/java/pl/pjatk/gameplay/GameplayApplicationTests.java
@@ -8,7 +8,6 @@ class GameplayApplicationTests {
 
 	@Test
 	void contextLoads() {
-
 	}
 
 }
